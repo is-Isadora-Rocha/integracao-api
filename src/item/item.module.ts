a2dcb1743e09ca26280f2c154804cccce557d850
is-Isadora-Rocha <is.isadora.rocha@gmail.com>
2024-11-22 13:36:42 -0300

    typeorm mysql e postgresql default

--- src/item/item.module.ts
+++ src/item/item.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { Item } from './item.entity';
 import { ItemService } from './item.service';
 import { ItemController } from './item.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   providers: [ItemService],
-  controllers: [ItemController]
+  controllers: [ItemController],
+  imports: [
+    TypeOrmModule.forFeature([Item], 'default'), //postgresql
+    TypeOrmModule.forFeature([Item], 'mysqlConnection') //mysql
+  ]
 })
 export class ItemModule {}
