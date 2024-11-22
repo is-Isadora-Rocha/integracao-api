cbffe84caf2aaf1ecbcc07ceef0877956f2e9aa1
is-Isadora-Rocha <is.isadora.rocha@gmail.com>
2024-11-22 13:12:22 -0300

    modulos de conexão

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,34 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    //conexão postgresql
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'root',
+      password: '12345678',
+      database: 'banco_pg',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    //conexão mysql
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '12345678',
+      database: 'banco_my',
+      name: 'mysqlConnection',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
