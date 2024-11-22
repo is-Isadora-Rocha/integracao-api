7a06b2dbb43ef4b6110ed469e1b6fb950535be83
is-Isadora-Rocha <is.isadora.rocha@gmail.com>
2024-11-22 13:36:57 -0300

    injeção dos bancos

--- src/item/item.service.ts
+++ src/item/item.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { Item } from './item.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class ItemService {}
+export class ItemService {
+    constructor(
+        @InjectRepository(Item, 'default')
+        private readonly postgresRepository: Repository<Item>,
+        @InjectRepository(Item, 'mysqlConnection')
+        private readonly mysqlRepository: Repository<Item>,
+    ) {}
+
+    async create(item: Item) {
+        await this.postgresRepository.save(item);
+        await this.mysqlRepository.save(item);
+        return item;
+    }
+
+    async findAll() {
+        const postgresItems = await this.postgresRepository.find();
+        const mysqlItems = await this.mysqlRepository.find();
+        return { postgresItems, mysqlItems };
+    }
+}
